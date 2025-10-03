b9187bcd931e93a5cb553f5314212bbd60138cc3
Toniownn <anthonycarl56@gmail.com>
2025-10-03 18:27:08 +0800

    feat: add images to e-lib project

--- src/assets/Images.tsx
+++ src/assets/Images.tsx
@@ -6,7 +6,28 @@ import entranceLoginSystem5 from "./entranceLoginSystem/entranceLoginSystem5.png
 import entranceLoginSystem6 from "./entranceLoginSystem/entranceLoginSystem6.png";
 import entranceLoginSystem7 from "./entranceLoginSystem/entranceLoginSystem7.png";
 import entranceLoginSystem8 from "./entranceLoginSystem/entranceLoginSystem8.png";
-import eLibSystem1 from "./eLibrarySystem/eLibSystem.png";
+import eLibSystem1 from "./eLibrarySystem/eLibSystem1.png";
+import eLibSystem2 from "./eLibrarySystem/eLibSystem2.png";
+import eLibSystem3 from "./eLibrarySystem/eLibSystem3.png";
+import eLibSystem4 from "./eLibrarySystem/eLibSystem4.png";
+import eLibSystem5 from "./eLibrarySystem/eLibSystem5.png";
+import eLibSystem6 from "./eLibrarySystem/eLibSystem6.png";
+import eLibSystem7 from "./eLibrarySystem/eLibSystem7.png";
+import eLibSystem8 from "./eLibrarySystem/eLibSystem8.png";
+import eLibSystem9 from "./eLibrarySystem/eLibSystem9.png";
+import eLibSystem10 from "./eLibrarySystem/eLibSystem10.png";
+import eLibSystem11 from "./eLibrarySystem/eLibSystem11.png";
+import eLibSystem12 from "./eLibrarySystem/eLibSystem12.png";
+import eLibSystem13 from "./eLibrarySystem/eLibSystem13.png";
+import eLibSystem14 from "./eLibrarySystem/eLibSystem14.png";
+import eLibSystem15 from "./eLibrarySystem/eLibSystem15.png";
+import eLibSystem16 from "./eLibrarySystem/eLibSystem16.png";
+import eLibSystem17 from "./eLibrarySystem/eLibSystem17.png";
+import eLibSystem18 from "./eLibrarySystem/eLibSystem18.png";
+import eLibSystem19 from "./eLibrarySystem/eLibSystem19.png";
+import eLibSystem20 from "./eLibrarySystem/eLibSystem20.png";
+import eLibSystem21 from "./eLibrarySystem/eLibSystem21.png";
+import eLibSystem22 from "./eLibrarySystem/eLibSystem22.png";
 import camm1 from "./camm/camm1.png";
 import camm2 from "./camm/camm2.png";
 import camm3 from "./camm/camm3.png";
@@ -28,7 +49,30 @@ export const entranceLoginSystem = [
   entranceLoginSystem8,
 ];
 
-export const eLibSystem = [eLibSystem1];
+export const eLibSystem = [
+  eLibSystem1,
+  eLibSystem2,
+  eLibSystem3,
+  eLibSystem4,
+  eLibSystem5,
+  eLibSystem6,
+  eLibSystem7,
+  eLibSystem8,
+  eLibSystem9,
+  eLibSystem10,
+  eLibSystem11,
+  eLibSystem12,
+  eLibSystem13,
+  eLibSystem14,
+  eLibSystem15,
+  eLibSystem16,
+  eLibSystem17,
+  eLibSystem18,
+  eLibSystem19,
+  eLibSystem20,
+  eLibSystem21,
+  eLibSystem22,
+];
 
 export const camm = [
   camm1,
